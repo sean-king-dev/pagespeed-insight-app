88654be21ad4e50a9579e2e1f50bd56fb30c43f8
Sean King <sean.king@kingseducation.com>
2024-03-08 10:58:34 +0000

    backup repo

---
+++ _misc/config.js
@@ -0,0 +1,5 @@
+const api = {
+  "apiKey": "zaSyD3_W1VvDbcrfxfQCxEfoSZYUz-SxAEfVoI",
+  "apiSecret": "your-api-secret",
+  "otherConfig": "some-value"
+}
---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+
+class Config:
+    # API_KEY = "zaSyD3_W1VvDbcrfxfQCxEfoSZYUz-SxAEfVoI"
+    API_KEY = "AIzaSyDlzonoPR5QnyEfL_T1Od7KjmNEgyHG2K0"
+    API_SECRET = "your-api-secret"
+    OTHER_CONFIG = "some-value"

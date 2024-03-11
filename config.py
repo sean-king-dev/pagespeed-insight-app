eb471792b456d1fb4d8de213b05382066a460cc8
Sean King <sean.king@kingseducation.com>
2024-03-11 09:39:26 +0000

    removed env variable files

--- config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-
-class Config:
-    # API_KEY = "zaSyD3_W1VvDbcrfxfQCxEfoSZYUz-SxAEfVoI"
-    # API_KEY = "AIzaSyDlzonoPR5QnyEfL_T1Od7KjmNEgyHG2K0"
-    API_KEY = "AIzaSyAO_xXdcgbC56kkIIRncHHT1LCWDi-kQj0"
-    API_SECRET = "your-api-secret"
-    OTHER_CONFIG = "some-value"
